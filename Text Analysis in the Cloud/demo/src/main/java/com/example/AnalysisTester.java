c9f5d1c367290a462c1349d6b8e3d917e7a7cbf9
MostafaTaha04 <mostafataha1804@gmail.com>
2025-11-15 18:01:15 +0200

    test

--- Text Analysis in the Cloud/demo/src/main/java/com/example/AnalysisTester.java
+++ Text Analysis in the Cloud/demo/src/main/java/com/example/AnalysisTester.java
@@ -3,60 +3,72 @@ package com.example; // Or your package name
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP; // Correct import
 import edu.stanford.nlp.semgraph.SemanticGraph;
 import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations;
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.TreeCoreAnnotations;
 import edu.stanford.nlp.util.CoreMap;
-import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-
 
 import java.util.Properties;
 
 /**
  * A simple class to test the Stanford Parser locally, as recommended
- * in the assignment's "Getting Started" section.
+ * in the assignment's "Getting Started" section [cite: 148-149].
+ *
+ * EFFICIENT VERSION: Creates only one pipeline.
  */
 public class AnalysisTester {
 
+    // We don't even need the static variable, we can
+    // just pass the pipeline as a parameter.
+    
     public static void main(String[] args) {
         // This is the text we will analyze.
         String textToAnalyze = "The quick brown fox jumps over the lazy dog.";
 
+        // --- 1. Create ONE pipeline for ALL tasks ---
+        // We include all annotators we'll need:
+        // 'pos' for POS
+        // 'parse' for Constituency (also needs pos)
+        // 'depparse' for Dependency (also needs pos)
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize, ssplit, pos, parse, depparse");
+        
+        System.out.println("Loading Stanford CoreNLP models... (This may take a moment)");
+        // This is the only time we create the heavy object.
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+        System.out.println("Models loaded.");
+
+        System.out.println("---");
         System.out.println("Starting analysis for text: \"" + textToAnalyze + "\"");
         System.out.println("---");
 
-        // --- 1. Test POS (Part-of-Speech) Analysis ---
-        //
+        // --- 2. Create the Annotation object ONCE ---
+        // The Annotation object will hold the results of all analyses.
+        Annotation document = new Annotation(textToAnalyze);
+        pipeline.annotate(document);
+
+        // --- 3. Run all tests using the SAME annotated document ---
         System.out.println("### 1. POS (Part-of-Speech) TAGGING ###");
-        performPosTagging(textToAnalyze);
+        // We just need to read the results from the document
+        performPosTagging(document);
         System.out.println("---------------------------------------");
 
-        // --- 2. Test Constituency Analysis ---
-        //
         System.out.println("### 2. CONSTITUENCY PARSING ###");
-        performConstituencyParsing(textToAnalyze);
+        performConstituencyParsing(document);
         System.out.println("---------------------------------------");
         
-        // --- 3. Test Dependency Analysis ---
-        //
         System.out.println("### 3. DEPENDENCY PARSING ###");
-        performDependencyParsing(textToAnalyze);
+        performDependencyParsing(document);
         System.out.println("---------------------------------------");
     }
 
     /**
      * Performs Part-of-Speech tagging.
+     * We pass the *annotated document* so we don't re-run the pipeline.
      */
-    private static void performPosTagging(String text) {
-        // Create a pipeline with only the necessary annotators
-        Properties props = new Properties();
-        props.setProperty("annotators", "tokenize, ssplit, pos");
-        
-        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-        Annotation document = new Annotation(text);
-        pipeline.annotate(document);
-
+    private static void performPosTagging(Annotation document) {
         // Loop over sentences
         for (CoreMap sentence : document.get(CoreAnnotations.SentencesAnnotation.class)) {
             // Loop over tokens (words)
@@ -71,16 +83,7 @@ public class AnalysisTester {
     /**
      * Performs Constituency parsing.
      */
-    private static void performConstituencyParsing(String text) {
-        // Create a pipeline that includes the parser
-        Properties props = new Properties();
-        // This pipeline will also do POS tagging, which is required for parsing
-        props.setProperty("annotators", "tokenize, ssplit, pos, parse"); 
-        
-        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-        Annotation document = new Annotation(text);
-        pipeline.annotate(document);
-
+    private static void performConstituencyParsing(Annotation document) {
         // Loop over sentences and get the parse tree
         for (CoreMap sentence : document.get(CoreAnnotations.SentencesAnnotation.class)) {
             Tree tree = sentence.get(TreeCoreAnnotations.TreeAnnotation.class);
@@ -92,15 +95,7 @@ public class AnalysisTester {
     /**
      * Performs Dependency parsing.
      */
-    private static void performDependencyParsing(String text) {
-        // Create a pipeline that includes the dependency parser
-        Properties props = new Properties();
-        props.setProperty("annotators", "tokenize, ssplit, pos, depparse"); 
-        
-        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-        Annotation document = new Annotation(text);
-        pipeline.annotate(document);
-
+    private static void performDependencyParsing(Annotation document) {
         // Loop over sentences and get the dependencies
         for (CoreMap sentence : document.get(CoreAnnotations.SentencesAnnotation.class)) {
             SemanticGraph dependencies = sentence.get(
