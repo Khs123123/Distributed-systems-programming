c44c0de16e12b7ebd8867c39f41f84008d3f265f
Khs123123 <Kh0508391231@gmail.com>
2025-11-15 17:57:39 +0200

    5

--- Text Analysis in the Cloud/demo/src/main/java/com/example/Localapplication.java
+++ Text Analysis in the Cloud/demo/src/main/java/com/example/Localapplication.java
@@ -1,11 +1,6 @@
 package com.example;
 
-<<<<<<< HEAD
-public class Localapplication {
-=======
-import software.amazon.awssdk.services.ec2.model.Tag;
 
-public class LocalApplication {
->>>>>>> 6f377ae3dffd869faf8fd44d1212d9e64930cae8
+public class Localapplication {
     
 }
